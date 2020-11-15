eaeae406a7954637901a1aebe3de2e5e7e971c9b
logicalsingular <logicalsingular@gmail.com>
2020-11-15 20:45:10 +0200

    [add] entity, field methods, game method

--- snake.py
+++ snake.py
@@ -2,39 +2,56 @@ from os import system, name
 from time import sleep 
 from random import randint
 
-class Field():
-    def __init__(self,w,l):
-        self.map = []
-        self.width = w
-        self.length = l
-    def get(self):
-        self.map = ["#"+['x']*self.length+"#"]*self.width
-        return self.map
-
-class Snake():
-    def __init__(self,x,y):
-        self.head_x = x
-        self.head_y = y
-        self.symbol = 'O' 
-        self.length = 1
-
-class Apple():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.symbol = '@'
-
-class Game():
-    def __init__(self):
-        self.field = Field(5,5)
-    def play_game(self):
-        print("Game started!")
-
 def clear():
     if name == "nt":
         system('cls')
     else:
         system('clear')
+
+class Field():
+    def __init__(self,w,l):
+        self.map = []
+        self.width = w
+        self.length = l
+
+    def background(self):
+        # self.map = [["#"]+['x']*self.length+["#"]]*self.width
+        self.map = [['x']*self.length]*self.width
+        print(self.map)
+    def add_entity(self,x,y,s):
+        self.map[14][24] = s
+
+    def draw(self):
+        for x in range(self.width):
+            for y in range(self.length):
+                print(self.map[x][y],end = "")
+            print("")
+        print(self.map)
+
+class Entity():
+    def __init__(self,x,y,s):
+        self.x = x
+        self.y = y
+        self.symbol = s 
+
+class Game():
+    def __init__(self):
+        self.field = Field(25,25)
+        self.live = True
+
+    def play_game(self):
+        print("Game started!")
+        while True:
+            response = input("Press any key to start or \"x\" for exit: ")
+            if response == "x":
+                break
+            head = Entity(12,12,"O")
+            apple = Entity(3,3, "@")
+            self.field.background()
+            self.field.add_entity(head.x,head.y,head.symbol)
+            # self.field.add_entity(apple.x,apple.y,apple.symbol)
+            self.field.draw()
+            exit()
 
 test = Game()
 test.play_game()
